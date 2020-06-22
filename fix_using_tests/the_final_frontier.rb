a4ef4f76ca580ca5a3505ec7954c60bcda3be9e6
Margarita Morozova <ritamorozova@yahoo.com>
2020-06-22 22:09:43 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -1,14 +1,14 @@
 puts# don't forget to add: require 'pry'
-require 'pry'
+
 
 def generate_star_date
-  binding.pry
+
   (rand(100000) + 400000) / 10.0
 end
 
 
 def state_log(star_date)
-  binding.pry
+ 
   "Captain's Log, star date #{star_date}."
 end
 
