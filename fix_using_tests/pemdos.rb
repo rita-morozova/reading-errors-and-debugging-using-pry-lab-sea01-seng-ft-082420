91203720313b4a475a736540f8c0b9851f6696d4
Margarita Morozova <ritamorozova@yahoo.com>
2020-06-22 22:41:23 +0000

    Done.

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,8 +1,8 @@
 # don't forget to add: require 'pry'
-
-def snake_it_up(string)
+def snake_it_up (string)
+  ten_s = "ssssssssss"
   if string[0] == "s"
-    10 * "s" + string
+    return ten_s + string
   else
     string
   end
