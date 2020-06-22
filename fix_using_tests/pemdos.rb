d27a385ab17657061eb97761f7e3dcfbd28d283f
Margarita Morozova <ritamorozova@yahoo.com>
2020-06-22 22:28:12 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -2,10 +2,7 @@
 require 'pry'
 def snake_it_up (string)
   if string[0] == "s"
-    ten_s = 10.times do
-             print "s"
-           end
-    puts ten_s + "string"
+    puts "ssssssssss" + "string"
   else
     "string"
   end
